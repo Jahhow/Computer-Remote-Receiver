0d9588624d1f5362687b41ae1fce2f53882d8fda
涂 家 浩 <35959817+Jahhow@users.noreply.github.com>
2020-01-07 23:40:52 +0800

    Connecting Logic

--- Remote Control Receiver/main.cpp
+++ Remote Control Receiver/main.cpp
@@ -11,7 +11,76 @@
 // #pragma comment (lib, "Mswsock.lib")
 
 #define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "1597"
+#define DEFAULT_PORT 1597
+#define DEFAULT_PORT_STR "1597"
+
+struct ServerHeader
+{
+	char Password[4] = { 'U', 'E', 'R', 'J' };
+	int Version = htonl(1);
+} serverHeader;
+
+u_short boundPort;
+bool connected = false;
+HANDLE eventConnected;
+
+DWORD WINAPI IpPortPrintingThread(LPVOID lpParam) {
+	printf("IpPortPrintingThread\n");
+
+	int iResult;
+	addrinfo* result = NULL;
+	addrinfo hints;
+	HANDLE StdoutHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD coord = { 0,0 };
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;// IPv4
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+
+	while (1) {
+		if (connected) {
+			system("cls");
+			puts("\n  Connected.");
+			iResult = SuspendThread(GetCurrentThread());
+			if (iResult == -1) {
+				printf("SuspendThread failed with error: %d\n", GetLastError());
+				WSACleanup();
+				ExitProcess(1);
+			}
+			connected = false;
+		}
+		else {
+			// Resolve the server address and port
+			iResult = getaddrinfo("", DEFAULT_PORT_STR, &hints, &result);
+			if (iResult != 0) {
+				printf("getaddrinfo failed with error: %d\n", iResult);
+				WSACleanup();
+				ExitProcess(1);
+			}
+
+			system("cls");
+			//SetConsoleCursorPosition(StdoutHandle, coord);
+
+			printf(
+				"Port: %hu\n"
+				"IP:\n", boundPort);
+			char ipv4str[16];
+			addrinfo* ai = result;
+			u_int i = 1;
+			while (ai) {
+				inet_ntop(ai->ai_family, &((sockaddr_in*)ai->ai_addr)->sin_addr, ipv4str, 16);
+				printf("  (%u) %s\n", i, ipv4str);
+				ai = ai->ai_next;
+				++i;
+			}
+			freeaddrinfo(result);
+			//Sleep(1000);
+			WaitForSingleObject(eventConnected, 1000);
+		}
+	}
+}
 
 int __cdecl main(void)
 {
@@ -21,8 +90,6 @@ int __cdecl main(void)
 	SOCKET ListenSocket = INVALID_SOCKET;
 	SOCKET ClientSocket = INVALID_SOCKET;
 
-	struct addrinfo* result = NULL;
-	struct addrinfo hints;
 
 	int iSendResult;
 	char recvbuf[DEFAULT_BUFLEN];
@@ -35,44 +102,77 @@ int __cdecl main(void)
 		return 1;
 	}
 
-	ZeroMemory(&hints, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_protocol = IPPROTO_TCP;
-	hints.ai_flags = AI_PASSIVE;
-
-	// Resolve the server address and port
-	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
-	if (iResult != 0) {
-		printf("getaddrinfo failed with error: %d\n", iResult);
-		WSACleanup();
-		return 1;
-	}
-
 	// Create a SOCKET for connecting to server
-	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	ListenSocket = socket(AF_INET/*IPv4*/, SOCK_STREAM, IPPROTO_TCP);
 	if (ListenSocket == INVALID_SOCKET) {
 		printf("socket failed with error: %ld\n", WSAGetLastError());
-		freeaddrinfo(result);
 		WSACleanup();
 		return 1;
 	}
 
+	sockaddr_in sa;
+	sa.sin_family = AF_INET;
+	sa.sin_addr = in4addr_any;
+	sa.sin_port = htons(DEFAULT_PORT);
+	ZeroMemory(&sa.sin_zero, sizeof(sa.sin_zero));
+
 	// Setup the TCP listening socket
-	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	iResult = bind(ListenSocket, (sockaddr*)&sa, sizeof(sa));
 	if (iResult == SOCKET_ERROR) {
-		printf("bind failed with error: %d\n", WSAGetLastError());
-		freeaddrinfo(result);
+		sa.sin_port = 0;// Auto pick available port
+
+		iResult = bind(ListenSocket, (sockaddr*)&sa, sizeof(sa));
+		if (iResult == SOCKET_ERROR) {
+			printf("bind failed with error: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+	}
+
+	if (sa.sin_port == 0) {
+		// Need to fetch port bound
+		int size = sizeof(sa);
+		iResult = getsockname(ListenSocket, (sockaddr*)&sa, &size);
+		if (iResult == SOCKET_ERROR) {
+			printf("getsockname failed with error: %d\n", WSAGetLastError());
+			closesocket(ListenSocket);
+			WSACleanup();
+			return 1;
+		}
+	}
+	boundPort = ntohs(sa.sin_port);
+
+	iResult = listen(ListenSocket, SOMAXCONN);
+	if (iResult == SOCKET_ERROR) {
+		printf("listen failed with error: %d\n", WSAGetLastError());
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
 	}
 
-	freeaddrinfo(result);
+	eventConnected = CreateEvent(0, false, 0, NULL);
+	if (eventConnected == NULL) {
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
 
-	iResult = listen(ListenSocket, SOMAXCONN);
-	if (iResult == SOCKET_ERROR) {
-		printf("listen failed with error: %d\n", WSAGetLastError());
+	DWORD MyThreadFunctionID;
+	HANDLE handleIpPortPrintingThread = CreateThread(
+		NULL,                   // default security attributes
+		0,                      // use default stack size  
+		IpPortPrintingThread,       // thread function name
+		NULL,                   // argument to thread function 
+		0,                      // use default creation flags 
+		&MyThreadFunctionID);   // returns the thread identifier 
+
+	// Check the return value for success.
+	// If CreateThread fails, terminate execution. 
+	// This will automatically clean up threads and memory. 
+	if (handleIpPortPrintingThread == NULL)
+	{
+		puts("CreateThread IpPortPrintingThread failed");
 		closesocket(ListenSocket);
 		WSACleanup();
 		return 1;
@@ -83,14 +183,26 @@ int __cdecl main(void)
 		ClientSocket = accept(ListenSocket, NULL, NULL);
 		if (ClientSocket == INVALID_SOCKET) {
 			printf("accept failed with error: %d\n", WSAGetLastError());
-			closesocket(ListenSocket);
-			WSACleanup();
-			return 1;
+			break;
+		}
+
+		iSendResult = send(ClientSocket, (const char*)&serverHeader, sizeof(serverHeader), 0);
+		if (iSendResult == SOCKET_ERROR) {
+			printf("send failed with error: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			continue;
+		}
+
+		connected = true;
+		iResult = SetEvent(eventConnected);
+		if (iResult == 0) {
+			printf("SetEvent(eventConnected) failed with error: %d\n", GetLastError());
+			closesocket(ClientSocket);
+			break;
 		}
 
 		// Receive until the peer shuts down the connection
 		do {
-
 			iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
 			if (iResult > 0) {
 				printf("Bytes received: %d\n", iResult);
@@ -112,7 +224,7 @@ int __cdecl main(void)
 				closesocket(ListenSocket);
 				closesocket(ClientSocket);
 				WSACleanup();
-				return 1;
+				ExitProcess(1);
 			}
 
 		} while (iResult > 0);
@@ -121,19 +233,19 @@ int __cdecl main(void)
 		iResult = shutdown(ClientSocket, SD_SEND);
 		if (iResult == SOCKET_ERROR) {
 			printf("shutdown failed with error: %d\n", WSAGetLastError());
-			closesocket(ListenSocket);
 			closesocket(ClientSocket);
-			WSACleanup();
-			return 1;
+			break;
 		}
 
 		// cleanup
 		closesocket(ClientSocket);
+		connected = false;
+		ResetEvent(eventConnected);
+		ResumeThread(handleIpPortPrintingThread);
 	}
 
 	// No longer need server socket
 	closesocket(ListenSocket);
 	WSACleanup();
-
-	return 0;
+	ExitProcess(1);
 }
